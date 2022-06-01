0f5a6794c01c4356a90856aa82db240fc3150813
kaitdu <kai@tdu.com.vn>
2022-06-01 23:32:43 +0700

    question, answer (status:done)

--- src/main/java/com/nhom5/blogonline/apiblogonline/auth/security/WebSecurityConfig.java
+++ src/main/java/com/nhom5/blogonline/apiblogonline/auth/security/WebSecurityConfig.java
@@ -60,6 +60,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
       .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
       .authorizeRequests().antMatchers("/api/auth/**").permitAll()
       .antMatchers("/api/test/**").permitAll()
+      .antMatchers("api/blog/**").permitAll()
+      .antMatchers("api/answer/**").permitAll()
       .anyRequest().authenticated();
 
     http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
